63be91ba2fb3e215e082b0376ad4f872105e3573
GhostKellz <ckelley@ghostkellz.sh>
2026-01-17 14:11:21 -0500

    updated for zig 0.16.0-dev.2193+fc517bd01

--- .zig-cache/c/bec0fd5380ccad274abb5a4fcc96a8f5/options.zig
+++
@@ -1,5 +0,0 @@
-pub const jit_enabled: bool = true;
-pub const unicode_full: bool = true;
-pub const streaming_enabled: bool = true;
-pub const capture_groups: bool = true;
-pub const backtracking_enabled: bool = true;
